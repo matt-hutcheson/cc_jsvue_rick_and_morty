a3165795ba9a467ac93ae67c2420708b47cc1a69
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-21 15:12:42 +0100

    MVP completed

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,35 @@
   <div>
     <h1>Rick and Morty Characters</h1>
     <character-list :characters='characters'></character-list>
+    <show-character :character='selectedCharacter'></show-character>
   </div>
 </template>
 
 <script>
 import CharacterList from './components/CharacterList.vue';
+import ShowCharacter from './components/CharacterShowDetails';
 import {eventBus} from '@/main.js'
 
 export default {
   name: "app",
   data() {
     return {
-      characters: []
+      characters: [],
+      selectedCharacter: null
     }
   },
   mounted() {
     fetch('https://rickandmortyapi.com/api/character/')
     .then(result  => result.json())
     .then(characters => this.characters = characters['results']);
+
+    eventBus.$on('character-selected', (character) => {
+      this.selectedCharacter = character
+    })
   },
   components: {
-    'character-list': CharacterList
+    'character-list': CharacterList,
+    'show-character': ShowCharacter
   }
 }
 </script>
---
+++ src/components/CharacterShowDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <article v-if="character">
+      <h3>{{character.name}}</h3>
+      <p>Status: {{character.status}}</p>
+      <p>Species: {{character.species}}</p>
+  </article>
+</template>
+
+<script>
+export default {
+    name: 'show-character',
+    props: ['character']
+}
+</script>
+
+<style>
+
+</style>
